56bfee55ba3a4c00fea47d86670f66e9a265ab19
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-05-14 07:45:16 +1000

    Added basic Layout and Header components

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Layout from './components/UI/Layout';
 import CreateContact from './views/CreateContact';
 
 class Routes extends Component {
   render() {
     return (
       <Router>
-        <Route exact path="/create-contact" component={CreateContact} />
+        <Layout>
+          <Route exact path="/create-contact" component={CreateContact} />
+        </Layout>
       </Router>
     );
   }
