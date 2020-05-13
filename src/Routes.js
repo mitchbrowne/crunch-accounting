0a184c75653b3f01dde3ac4da92eb6d35d398330
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-05-13 21:44:19 +1000

    Added CreateContact view, included in routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import CreateContact from './views/CreateContact';
+
 class Routes extends Component {
   render() {
     return (
-      <div>
-        <h1>Welcome Home</h1>
-      </div>
+      <Router>
+        <Route exact path="/create-contact" component={CreateContact} />
+      </Router>
     );
   }
 }
