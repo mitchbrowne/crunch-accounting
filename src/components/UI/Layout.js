1eb75de6fcc519f190dece208bbc9cbc4e046f8e
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-05-14 09:26:09 +1000

    Added footer with styling

--- src/components/UI/Layout.js
+++ src/components/UI/Layout.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 import Header from './Header';
+import Footer from './Footer';
 
 export default (props) => {
   return (
     <div className="layout-wrapper">
       <Header />
-      {props.children}
+      <div className="content-wrapper">
+        <div>
+          {props.children}
+        </div>
+        <Footer />
+      </div>
     </div>
   )
 }
