1c62517a01b2b4843bf50fdff26e667b9885b7e7
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-05-13 21:28:36 +1000

    Removed unnecessary create react app files. Replaced App.js with Routes.js. Added basic folder structure.

---
+++ src/Routes.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+class Routes extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome Home</h1>
+      </div>
+    );
+  }
+}
+
+export default Routes;
