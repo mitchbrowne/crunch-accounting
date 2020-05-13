9fc7e41db0c7aa1fdfd1f3f7a0a9b7ccbc84d0d1
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-05-13 22:45:03 +1000

    Added basic form sections and corresponding fields

--- src/components/CreateContact/DescriptionInfo.js
+++ src/components/CreateContact/DescriptionInfo.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
+import {
+  Container,
+  Row,
+  Col,
+  Form
+} from 'react-bootstrap';
+
 export default () => {
   return (
-    <h3>Description Information</h3>
+    <Container>
+      <Row>
+        <Col>
+          <h3>Description Information</h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Form.Group controlId="description">
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              required
+              type="text"
+            />
+          </Form.Group>
+        </Col>
+      </Row>
+    </Container>
   )
 }
