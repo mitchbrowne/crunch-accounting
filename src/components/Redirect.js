55af3a8e8aa5293eb0fdc5aac055a785bd481655
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-05-14 17:43:31 +1000

    Renamed ContactFormLong to ContactForm. Refactored Redirect

--- src/components/Redirect.js
+++ src/components/Redirect.js
@@ -1,10 +1,4 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 
-export default () => {
-
-
-  return (
-      <Redirect to='/create-contact' />
-  )
-}
+export default () => <Redirect to='/create-contact' />;
